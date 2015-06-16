6addec0bcc303f6f57cc33186b88dd933ebe51b5
lizeth <lizethvalencia97@gmail.com>
2015-06-15 19:47:40 -0500

    subi el archivo de la clase abstracta

---
+++ ClaseAbstracta.php
@@ -0,0 +1,27 @@
+<?php
+abstract class ClaseAbstracta
+{
+    //metodo con los argumentos requeridos
+    abstract protected function nombrePrefijo($nombre);
+
+}
+
+class ClaseConcreta extends ClaseAbstracta
+{
+
+    public function nombrePrefijo($nombre) {
+        if ($nombre == "Carlos") {
+            $prefijo = "Mr";
+        } elseif ($nombre == "Carlota") {
+            $prefijo = "Mrs";
+        } else {
+            $prefijo = "";
+        }
+        return "{$prefijo} {$nombre}";
+    }
+}
+
+$clase = new ClaseConcreta;
+echo $clase->nombrePrefijo("Carlos"), "\n";
+echo $clase->nombrePrefijo("Carlota"), "\n";
+?>
